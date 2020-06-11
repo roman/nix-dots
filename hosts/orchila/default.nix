07da4b2641a7695f51fc8e687aa54e4fbcd5b627
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-06-11 00:21:34 -0700

    Add passwordless sudo to roman on orchila

--- hosts/orchila/default.nix
+++ hosts/orchila/default.nix
@@ -6,4 +6,14 @@ let
 in
   {
     imports =  do-userdata ++ [do-nixos-module dots-system];
+    security.sudo.extraConfig =
+    ''
+      Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
+      Defaults:root,%wheel env_keep+=NIX_PATH
+      Defaults:root,%wheel env_keep+=TERMINFO_DIRS
+      Defaults env_keep+=SSH_AUTH_SOCK
+      Defaults lecture = never
+      root   ALL=(ALL) SETENV: ALL
+      %wheel ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
+    '';
   }
