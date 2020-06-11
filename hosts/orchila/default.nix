736ac998adccf644168c7a31d8aa28699d419149
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-06-11 00:16:09 -0700

    Fix orchila config

--- hosts/orchila/default.nix
+++ hosts/orchila/default.nix
@@ -1,11 +1,9 @@
 { modulesPath, lib, ... }:
 let
-  hostname        = "orchila";
-  do-userdata     = lib.optional (builtins.pathExists ./do-userdata.nix);
+  do-userdata     = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix;
   do-nixos-module = modulesPath + "/virtualisation/digital-ocean-config.nix";
   dots-system   = ../../system;
 in
   {
     imports =  do-userdata ++ [do-nixos-module dots-system];
-    networking.hostName = hostname;
   }
