6e35547f8d8e3ab0e48f00df16b309e75789d9ee
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-06-13 20:08:42 +0000

    Pimp terminal session

--- home/base.nix
+++ home/base.nix
@@ -6,6 +6,9 @@ with pkgs;
 
   programs = {
 
+    zoxide.enable = true;
+    man.enable = true;
+
     bat = {
       enable = true;
       config = {
@@ -26,23 +29,59 @@ with pkgs;
       ls = "exa -a --git-ignore";
       tree = "exa --tree --git-ignore";
     };
+
+    bash = {
+      enable = true;
+      shellAliases = {
+        fzp = ''fzf --preview "bat --color=always {}"'';
+        cat = "bat";
+        ll = "exa -lagFT --git-ignore --git --level 1";
+        tree = "exa --tree --git-ignore";
+      };
+
+    };
+
+    starship = {
+      enable = true;
+      enableBashIntegration = true;
+      settings = {
+        git_commit.only_detached = false;
+        git_commit.commit_hash_length = 8;
+      };
+    };
+
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      enableNixDirenvIntegration = true;
+    };
+
   };
  
   home.packages = [
     (aspellWithDicts (ps: with ps; [ en es ]))
+
+    figlet
+
+    git
+    gitAndTools.hub
+
+    vim
     jq
+
     exa
-    niv
-    nixfmt
     fd
     ripgrep
-    nix-prefetch-git
-    nix-prefetch-github
-    figlet
+
     htop
     ytop
+
     wget
     curl
-  ];
 
+    niv
+    nixfmt
+    nix-prefetch-git
+    nix-prefetch-github
+  ];
 }
