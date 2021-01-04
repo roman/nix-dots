c9271c615faf216e036d2ed43a06af123163bd7c
Roman Gonzalez <open-source@roman-gonzalez.info>
2021-01-04 21:51:17 +0000

    add nixbox config

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    homeManager.url = "github:nix-community/home-manager";
+    homeManager.url = "github:nix-community/home-manager/release-20.09";
     homeManager.inputs.nixpkgs.follows = "nixpkgs";
 
     emacs.url = "github:nix-community/emacs-overlay";
@@ -28,24 +28,51 @@
       # buildVagrantGuest is a function that creates a configuration that is
       # vagrant guest friendly (e.g. ubuntu machine in vagrant)
       buildVagrantGuest = username:
-        homeManager.lib.homeManagerConfiguration {
-          inherit username;
-          system = "x86_64-linux";
-          homeDirectory = "/home/${username}";
-          configuration = args: {
-            imports = [ ./home ];
-            nixpkgs = {
-              overlays = [ emacs.overlay hm-lib guest-zoo-packages ];
-              config = { allowUnfree = true; };
-            };
+        homeManager.lib.homeManagerConfiguration (buildHomeManagerPlainConfig username);
+
+      buildHomeManagerPlainConfig = username: {
+        inherit username;
+        system = "x86_64-linux";
+        homeDirectory = "/home/${username}";
+        configuration = args: {
+          home.stateVersion = "20.09";
+          imports = [ ./home ];
+          nixpkgs = {
+            overlays = [ emacs.overlay hm-lib guest-zoo-packages ];
+            config = { allowUnfree = true; };
           };
         };
+      };
+
     in
       {
+
         homeManagerConfigurations = {
           # normally ubuntu and vagrant are names found in vagrant images
           ubuntu-guest = buildVagrantGuest "ubuntu";
           vagrant-guest = buildVagrantGuest "vagrant";
         };
+
+        nixosConfigurations = {
+          nixbox = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules =
+              [
+                ./hosts/nixbox
+                homeManager.nixosModules.home-manager
+                {
+                  home-manager.users.vagrant = {
+                    home.stateVersion = "20.09";
+                    nixpkgs = {
+                      overlays = [ emacs.overlay hm-lib guest-zoo-packages ];
+                      config = { allowUnfree = true; };
+                    };
+                    imports = [ ./home ];
+                  };
+                }
+              ];
+          };
+        };
+
       };
 }
