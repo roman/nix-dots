6b49e891ad195d9d63bd34628ac6beee1d3e9d15
Roman Gonzalez <open-source@roman-gonzalez.info>
2021-06-06 01:56:32 -0700

    build(nix): add darwin input and macos configuration for westeros

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     homeManager.url = "github:nix-community/home-manager/release-21.05";
     homeManager.inputs.nixpkgs.follows = "nixpkgs";
@@ -9,11 +11,12 @@
     emacsOverlay.url = "github:nix-community/emacs-overlay/f6768d390c0c6033735a7538150131fea8518ad6";
   };
 
-  outputs = inputs@{ self, nixpkgs, homeManager, emacsOverlay }:
+  outputs = inputs@{ self, nixpkgs, darwin, homeManager, emacsOverlay }:
 
     let
 
       homeModules = import ./config/home-manager inputs;
+      hm = import ./lib/hm.nix;
       vagrant = import ./lib/vagrant.nix inputs;
       flakeOverlay = final: prev: {
         keytar = import ./nix/keytar prev;
@@ -21,6 +24,27 @@
 
     in {
 
+      darwinConfigurations.westeros = darwin.lib.darwinSystem {
+        modules = [
+          homeManager.darwinModules.home-manager
+          {
+            home-manager.useUserPackages = true;
+          }
+          ({ pkgs, ... }: {
+            home-manager.users.roman = hm.buildHomeManagerConfig {
+              modules = with homeModules; [
+                bash
+                emacs
+                git
+                nix-utils
+              ];
+              overlays = [
+                emacsOverlay.overlay
+              ];
+            };
+          })
+        ];
+      };
 
       homeManagerConfigurations = {
         # normally ubuntu and vagrant are names found in vagrant images
